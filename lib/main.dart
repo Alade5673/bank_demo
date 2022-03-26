fac230c52822e89863328bfa1e06dc4a5d6cd353
FALANA ELIJAH <falanaelijah@FALANAs-MacBook-Pro.local>
2022-03-26 14:13:02 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:bank_app/screens/TransactionsScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          backgroundColor: Color.fromRGBO(39, 50, 80, 1),
+
+          appBar: AppBar(
+            title: Text("TRANSACTIONS"),
+            centerTitle: true,
+            backgroundColor: Colors.pinkAccent,
+          ),
+
+          body: TransactionsScreen(),
+        )
+    );
+  }
+}
+
